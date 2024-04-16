70ec73613d9c18750052df9d773d47d506bffd69
imworkingondying <167254465+imworkingondying@users.noreply.github.com>
2024-04-17 01:18:16 +0300

    Delete darksouls💀 directory

--- darksouls💀/Item.java
+++
@@ -1,7 +0,0 @@
-package DZ23;
-
-public interface Item {
-    String getName();
-    int getCost();
-    String getDescription();
-}
--- darksouls💀/OrderAlreadyException.java
+++
@@ -1,7 +0,0 @@
-package DZ23;
-
-public class OrderAlreadyException extends Exception{
-    public OrderAlreadyException(int ex){
-        super("Order is already added at " + ex + " table");
-    }
-}
